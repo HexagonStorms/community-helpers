f1948a33588307457d91c7a25a22c23cbb45d182
Frank Pigeon <frank.pigeonjr@gmail.com>
2014-07-15 15:40:21 -0500

    Created helpers_jobs_mapping and modified reviews table.

--- app/database/migrations/2014_07_15_201920_create_helpers_jobs_mapping.php
+++ app/database/migrations/2014_07_15_201920_create_helpers_jobs_mapping.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateReviewsTable extends Migration {
+class CreateHelpersJobsMapping extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,12 +12,14 @@ class CreateReviewsTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('reviews', function($table)
+		Schema::create('helpers_jobs_mapping', function($table)
 		{
+			$table->integer('user_id')->unsigned();
+		    $table->foreign('user_id')->references('id')->on('users');
 		    $table->integer('jobs_id')->unsigned();
 		    $table->foreign('jobs_id')->references('id')->on('jobs');
-		    $table->integer('ratings');
-		    $table->primary('job_id');
+		    $table->boolean('is_accepted');
+		    $table->primary(array('user_id', 'jobs_id'));
 			$table->timestamps();
 		});
 	}
@@ -29,8 +31,7 @@ class CreateReviewsTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('reviews');
+		Schema::drop('helpers_jobs_mapping');
 	}
-
 
 }
