9f6d069673603b540b18879beaa550322c6acb4e
HexagonStorms <josueplazamusic@gmail.com>
2014-07-16 10:55:45 -0500

    redesigned homepage, added images to about us, strengthen communities, and parents sections. Also added dummy texts

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -29,7 +29,39 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-12">
-                <h2> For parents </h2>
+                <h2>FOR PARENTS</h2>
+                <div class="row">
+                        <div class="col-sm-4 text-center">
+                            <h3 class="text-center">Safety</h3>
+                            <img class="adjustable-width" src="/css/images/community.jpg" />
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                        <div class="col-sm-4 text-center">
+                            <h3 class="text-center">Risk-Free</h3>
+                            <img class="adjustable-width" src="/css/images/teen.jpg" />
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                        <div class="col-sm-4 text-center">
+                            <h3 class="text-center">Reliable</h3>
+                            <img class="adjustable-width" src="/css/images/dishes.jpg" />
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                </div>
             </div>
         </div>
     </div>
@@ -40,6 +72,19 @@
         <div class="row">
             <div class="col-sm-12">
                 <h2> Strengthening Communities</h2>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <img class="adjustable-width" src="/css/images/strength.jpg" />
+                    </div>
+                    <div class="col-sm-6">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -50,6 +95,47 @@
         <div class="row">
             <div class="col-sm-12">
                 <h2> About Us </h2>
+                <div class="row">
+                        <div class="col-sm-4 text-center">
+                            
+                            <img class="adjustable-width-75 img-circle" src="/css/images/frank.jpg" />
+                            <br /><br />
+                            <div class="row">
+                                    <div class="col-sm-4"><span class="fa fa-linkedin-square fa-3x"></span></div>
+                                    <div class="col-sm-4"><span class="fa fa-github-square fa-3x"></span></div>
+                                    <div class="col-sm-4"><span class="fa fa-twitter-square fa-3x"></span></div>
+                            </div>
+
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua.</p>
+                        </div>
+                        <div class="col-sm-4 text-center">
+                            
+                            <img class="adjustable-width-75 img-circle" src="/css/images/jon.jpg" />
+                            <br /><br />
+                            <div class="row">
+                                    <div class="col-sm-4"><span class="fa fa-linkedin-square fa-3x"></span></div>
+                                    <div class="col-sm-4"><span class="fa fa-github-square fa-3x"></span></div>
+                                    <div class="col-sm-4"><span class="fa fa-twitter-square fa-3x"></span></div>
+                            </div>
+
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua.</p>
+                        </div>
+                        <div class="col-sm-4 text-center">
+                            
+                            <img class="adjustable-width-75 img-circle" src="/css/images/josue.jpg" />
+                            <br /><br />
+                            <div class="row">
+                                    <div class="col-sm-4"><span class="fa fa-linkedin-square fa-3x"></span></div>
+                                    <div class="col-sm-4"><span class="fa fa-github-square fa-3x"></span></div>
+                                    <div class="col-sm-4"><span class="fa fa-twitter-square fa-3x"></span></div>
+                            </div>
+
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua.</p>
+                        </div>
+                </div>
             </div>
         </div>
     </div>
