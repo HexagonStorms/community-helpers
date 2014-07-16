ea2a7ce2ebcdc86c7ff28ae052b413ac1d4e683a
HexagonStorms <josueplazamusic@gmail.com>
2014-07-16 09:48:17 -0500

    Added dummy text to page

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -6,32 +6,51 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-12">
-                <p> video + who we are </p>
+                <div class="row">
+                    <div class="col-sm-6 col-xs-12">
+                        <h2>COMMUNITY HELPERS</H2>
+                        <p>Empowering San Antonio's workforce to help out in the community. Lorem 
+                        ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat.</p>
+                    </div>
+                    <div class="col-sm-6 col-xs-12">
+                        <iframe width="100%" height="315" src="//www.youtube.com/embed/tNZvNs6r800" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
 
-<div class="container">
-    <div class="row">
-        <div class="col-sm-12">
-            <p> For parents </p>
+<div class="sidekick">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h2> For parents </h2>
+            </div>
         </div>
     </div>
 </div>
 
-<div class="container">
-    <div class="row">
-        <div class="col-sm-12">
-            <p> Strengthening Communities</p>
+<div class="jumbotron">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h2> Strengthening Communities</h2>
+            </div>
         </div>
     </div>
 </div>
 
-<div class="container">
-    <div class="row">
-        <div class="col-sm-12">
-            <p> About Us </p>
+<div class="sidekick">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h2> About Us </h2>
+            </div>
         </div>
     </div>
 </div>
