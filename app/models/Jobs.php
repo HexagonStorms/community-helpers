d823dc09738270c8860422eea5c00810a6d5c8da
Frank Pigeon <frank.pigeonjr@gmail.com>
2014-07-16 11:10:14 -0500

    Created Jobs mode and controllerl

---
+++ app/models/Jobs.php
@@ -0,0 +1,46 @@
+<?php
+use Illuminate\Auth\UserTrait;
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableTrait;
+use Illuminate\Auth\Reminders\RemindableInterface;
+
+class Jobs extends Eloquent implements UserInterface, RemindableInterface{
+
+	// the db table this model realtes to
+    protected $table = 'jobs';
+
+    // validation rules for our model properties
+    static public $rules = [
+    	'category' => 'required|max:50',
+    	'description' => 'required|max:100',
+        'price' => 'required',
+        'description' => 'required|max:100',
+        'is_complete' => 'required',
+        'required_date' => 'required',
+        'required_time' => 'required',
+    ];
+
+ //    public function helpers()
+	// {
+ //    	return $this->belongsToMany('User');
+	// }// end of helpers
+
+ //    public function owner()
+ //    {
+ //        return $this->belongsTo('User');
+ //    }// end of helpers
+
+
+    // public function renderBody()
+    // {
+    //     $dirtyHTML = Parsedown::instance()->parse($this->description);
+    //     $config = HTMLPurifier_Config::createDefault();
+    //     $purifier = new HTMLPurifier($config);
+    //     return $purifier->purify($dirtyHTML);
+    // } //end of renderBody
+
+    // public function canManagePost()
+    // {
+    //     return Auth::check() && (Auth::user()->is_admin || Auth::user()->id == $this->user_id);
+    // }
+}
