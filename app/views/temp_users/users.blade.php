2c899209c3f647d34460f4b4b140f0dfc263c228
jonathan robinson <jonbrobinson@gmail.com>
2014-07-17 09:43:11 -0500

    refactored code to add user to database and display to show blade

--- app/views/temp_users/users.blade.php
+++ app/views/temp_users/users.blade.php
@@ -1,16 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Community Helpers">
-    <meta name="author" content="Frank Pigeon, Jon B. Robinson, Josue Plaza">
-    <link rel="shortcut icon" href="../../assets/ico/favicon.png">
+@extends('layouts.master')
 
-    <title>Test</title>
+@section('content')
+	{{ Form::open(array('action' => 'UsersController@store', 'role' => 'form', 'files' => true) )}}
+		<div><h3>Contact Info</h3></div>
+		<div class="form-group">
+			{{ Form::label('first_name', 'First Name') }}
+			{{ Form::text('first_name') }}<br>
 
-</head>
-<body>
-	<p>Test</p>
-</body>
+			{{ Form::label('last_name', 'Last Name') }}
+			{{ Form::text('last_name') }}<br>
+
+			{{ Form::label('email', 'Email') }}
+			{{ Form::email('email') }}<br>
+
+			{{ Form::label('password', 'Password') }}
+			{{ Form::password('password') }}<br>
+
+			{{ Form::label('birth_date', 'Date of Birth') }}
+			{{ Form::text('birth_date') }}<br>
+		</div>
+		<div><h3>Admin Info</h3></div>
+		<div>
+			{{ Form::label('is_helper', 'Are you a Helper?') }}
+			{{ Form::checkbox('is_helper', '1') }}
+
+			{{ Form::label('is_admin', 'Are you an Admin?') }}
+			{{ Form::checkbox('is_admin', '1') }}<br>
+		</div>
+		<div><h3>Address</h3></div>
+			{{ Form::label('street', 'Street') }}
+			{{ Form::text('street') }}<br>
+
+			{{ Form::label('city', 'city') }}
+			{{ Form::text('city') }}<br>
+
+			{{ Form::label('state', 'State') }}
+			{{ Form::text('state') }}<br>
+
+			{{ Form::label('zip', 'Zipcode') }}
+			{{ Form::text('zip') }}<br>
+
+		</div>
+		{{Form::submit('Save', ['class' => 'btn btn-success'])}}
+	{{ Form::close() }}
+@stop
