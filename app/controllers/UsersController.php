e6c78e51bc2ecd2ef9689bee0597c3b2e1bbea68
HexagonStorms <josueplazamusic@gmail.com>
2014-07-19 10:37:37 -0500

    Added new get to visit dashboard

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -65,7 +65,7 @@ class UsersController extends \BaseController {
 
 			Auth::loginUsingId($user->id);
 
-			return Redirect::action('UsersController@show', $user->id);
+			return Redirect::action('UsersController@dashboard', $user->id);
 		}
 	}
 
@@ -77,6 +77,17 @@ class UsersController extends \BaseController {
 	 * @return Response
 	 */
 	public function show($id)
+	{
+		
+		$user = User::findOrFail($id);
+		$data = array(
+			'user' => $user
+		);
+
+		return View::make('users.view_profile')->with($data);
+	}
+
+	public function dashboard($id)
 	{
 		$jobs = Job::with('creator')->orderBy('created_at', 'desc')->paginate(4);
 		$user = User::findOrFail($id);
