c6ff0ed62a13e613de7961350366caebee30fa17
HexagonStorms <josueplazamusic@gmail.com>
2014-07-21 14:09:42 -0500

    Prep redesign on show

--- app/views/temp_jobs/show.blade.php
+++ app/views/temp_jobs/show.blade.php
@@ -1,27 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Show Job</title>
-</head>
-<body>
-	<h1>Job Number: {{ $job->id}} </h1>
-	<h2>Category: {{ $job->category}} </h2>
-    <h5>Description: {{ $job->description }} </h5>
-    <h5>Cash: {{ $job->price }} </h5>
-    <h5>Date Needed: {{{ $job->required_date }}} </h5>
-    <h5>Created By: {{ $job->creator->first_name }} {{ $job->creator->last_name }}</h5>
+@extends('layouts.master')
 
-    @if(Auth::user()->is_helper == FALSE)
-        <!-- For giver to delete job -->
-        {{ Form::open(array('action' => array('JobsController@destroy', $job->id), 'method' => 'DELETE')) }}
-            <button type="submit" class="btn btn-sm btn-danger">Delete</button>
-        {{ Form::close() }}
-    @elseif(Auth::user()->is_helper == TRUE)
-        <!-- For helper to apply for job -->
-        {{ Form::open(array('action' => array('JobsController@apply', $job->id), 'method' => 'POST')) }}
-            <button type="submit" class="btn btn-sm btn-succces">Apply</button>
-        {{ Form::close() }}
-    @endif
-</body>
-</html>
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+        	<h1>Job Number: {{ $job->id}} </h1>
+        	<h2>Category: {{ $job->category}} </h2>
+            <h5>Description: {{ $job->description }} </h5>
+            <h5>Cash: {{ $job->price }} </h5>
+            <h5>Date Needed: {{{ $job->required_date }}} </h5>
+            <h5>Created By: {{ $job->creator->first_name }} {{ $job->creator->last_name }}</h5>
+
+            @if(Auth::user()->is_helper == FALSE)
+                <!-- For giver to delete job -->
+                {{ Form::open(array('action' => array('JobsController@destroy', $job->id), 'method' => 'DELETE')) }}
+                    <button type="submit" class="btn btn-sm btn-danger">Delete</button>
+                {{ Form::close() }}
+            @elseif(Auth::user()->is_helper == TRUE)
+                <!-- For helper to apply for job -->
+                {{ Form::open(array('action' => array('JobsController@apply', $job->id), 'method' => 'POST')) }}
+                    <button type="submit" class="btn btn-sm btn-succces">Apply</button>
+                {{ Form::close() }}
+            @endif
+
+        </div>
+    </div>
+</div>
+
+@stop
