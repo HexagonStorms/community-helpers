cc78da8040d46877fc44fd0974f8dd5dc9a51f47
HexagonStorms <josueplazamusic@gmail.com>
2014-07-21 10:32:39 -0500

    Redesigned with improved login page

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -1,23 +1,37 @@
 @extends('layouts.master')
 
 @section('content')
+
+<div class="container">
 	<div class="row">
 		<div class="col-md-4 col-md-offset-4 well">
-			 <img class="img-responsive  centered" src="/img/user.jpg"
-                    alt="">
-			<h2><i class="fa fa-user"></i> Sign In</h2>
+			<div class="row">
+				<div class="col-sm-12">
+			 		<img class="margin-auto img-responsive" src="/img/user.jpg"
+                    alt="Blank user image">
+            	</div>
+            </div>
+            <div class="row">
+            	<div class="col-sm-12">
+					<h2><i class="fa fa-user"></i> Sign In</h2>
+				</div>
+			</div>
 			<div class="form-group">
 				{{ Form::open(array('action' => array('HomeController@doLogin'), 'role'=> 'form')) 	}}
 					{{ Form::email('email', null, array('placeholder' => 'Email', "class" => "form-control")) }}
 					{{ Form::password('password', array('placeholder' => 'Password', "class" => "form-control")) }}
 					<div class="checkbox">
-					   <label>
-					     <input type="checkbox"> Remember Me
-					   </label>
-					 </div>
+					   <label><input type="checkbox"> Remember Me</label>
+					</div>
 					{{ Form::submit('Sign In', ['class' => 'btn btn-primary']) }}
 				{{ Form::close() }}
 			</div>
+			<div class="row">
+				<div class="col-sm-6 col-sm-offset-3">
+					<a href="/users/create"><p>Create an account</p></a>
+				</div>
+			</div>
 		</div>
 	</div>
+</div>
 @stop
