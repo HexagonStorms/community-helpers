b8d7f0a99dc218183a65112e85f530469cf163cb
jonathan robinson <jonbrobinson@gmail.com>
2014-07-21 15:40:31 -0500

    Added thumnail function

--- app/models/User.php
+++ app/models/User.php
@@ -35,6 +35,51 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 		'gender'=> 'max:1'
     ];
 
+    function makeThumbnails($updir, $img, $id,$MaxWe=300,$MaxHe=300)
+    {
+        $arr_image_details = getimagesize($img); 
+        $width = $arr_image_details[0];
+        $height = $arr_image_details[1];
+
+        $percent = 100;
+        if($width > $MaxWe) $percent = floor(($MaxWe * 100) / $width);
+
+        if(floor(($height * $percent)/100)>$MaxHe)  
+        $percent = (($MaxHe * 100) / $height);
+
+        if($width > $height) {
+            $newWidth=$MaxWe;
+            $newHeight=round(($height*$percent)/100);
+        }else{
+            $newWidth=round(($width*$percent)/100);
+            $newHeight=$MaxHe;
+        }
+
+        if ($arr_image_details[2] == 1) {
+            $imgt = "ImageGIF";
+            $imgcreatefrom = "ImageCreateFromGIF";
+        }
+        if ($arr_image_details[2] == 2) {
+            $imgt = "ImageJPEG";
+            $imgcreatefrom = "ImageCreateFromJPEG";
+        }
+        if ($arr_image_details[2] == 3) {
+            $imgt = "ImagePNG";
+            $imgcreatefrom = "ImageCreateFromPNG";
+        }
+
+
+        if ($imgt) {
+            $old_image = $imgcreatefrom($img);
+            $new_image = imagecreatetruecolor($newWidth, $newHeight);
+            imagecopyresized($new_image, $old_image, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+
+            $imgt($new_image, $updir."/".$id."_profile.jpg");
+
+            return "/".$updir."/".$id."_profile.jpg";
+        }
+    }
+
 	protected $hidden = array('password', 'remember_token');
 
 	public function reviews()
@@ -61,5 +106,15 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	public function setCityAttribute ($value)
 	{
 		$this->attributes['city'] = ucwords(strtolower($value));
+	}
+
+	public function setFirst_nameAttribute ($value)
+	{
+		$this->attributes['first_name'] = ucwords(strtolower($value));
+	}
+
+	public function setLast_nameAttribute ($value)
+	{
+		$this->attributes['last_name'] = ucwords(strtolower($value));
 	}
 }
