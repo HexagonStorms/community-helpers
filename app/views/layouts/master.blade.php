b9c50094a55e6bfa2db819f46161dc274f9990a6
HexagonStorms <josueplazamusic@gmail.com>
2014-07-29 15:49:23 -0500

    Google analytics

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -222,6 +222,7 @@
 	</div>
 
 
+
 	<!-- Bootstrap core JavaScript
 	================================================== -->
 	<!-- Placed at the end of the document so the pages load faster -->
@@ -240,6 +241,18 @@
 	</script>
 	
 	@yield('bottomscript')
+
+	<script>
+  	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  	ga('create', 'UA-30027930-3', 'auto');
+  	ga('send', 'pageview');
+
+</script>
+
 </body>
 </html>
 
